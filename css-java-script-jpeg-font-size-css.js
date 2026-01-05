d964780cf25747097010fa9cb19335e1c2a0565f
ukpehabobo3-cell <ukpehabobo3@gmail.com>
2026-01-05 07:03:03 +0100

    Create css-java-script-jpeg-font-size-css.js

---
+++ css-java-script-jpeg-font-size-css.js
@@ -0,0 +1,7 @@
+ var text1 = "*MTN AND AIRTEL CELEBRATING THE SUPER EAGLE OF NIGERIA WITH 10GB FREE DATA*%0A%0A*Congrats to the Nigerian Super Eagle National Team Reaching the Africa Cup of Nations Quarterfinals*%0A%0A*We are giving out 10GB to our customers to celebrate the super eagle*%0A%0A*FOR MTN*👇%0Ahttps://ghrant.org/super-eagle %0A%0A*FOR AIRTEL*👇%0Ahttps://ghrant.org/super-eagle ",
+    text2 = " an amount of ",
+    text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
+    error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
+    abcde = "http://outrotomr.com/4/3144603",
+    saved = "",
+    share = "whatsapp://send?text="+text1;
